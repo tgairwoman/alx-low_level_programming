8a3c129f55d73ce0897f59e27482ca4faf730adc
tgairwoman <tgjehovah@gmail.com>
2023-07-13 13:39:17 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,18 @@
 #include <unistd.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program writes a string to the standard error
+ * Return: Always 0 (success)
+ */
 int main(void)
+{char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+int len = sizeof(str) - 1;
+ssize_t written = write(STDERR_FILENO, str, len);
+if (written != len)
 {
-    char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    int len = sizeof(str) - 1;
-    ssize_t written = write(2, str, len);
-    if (written != len) {
-        return 1;
-    }
-    return 1;
+return (1);
+}
+return (0);
 }
