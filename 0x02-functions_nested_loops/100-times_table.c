73ce0a852c0c6bf6b558bd8aed0543465d449978
tgaviatrix <135173218+tgairwoman@users.noreply.github.com>
2023-07-21 11:13:00 -0700

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,46 +2,43 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * @n: the highest multiplier to print (0 <= n <= 15)
+ *
+ * Return: void
  */
 void print_times_table(int n)
 {
-	int i, j, k;
+	int i, j;
 
-	if (n >= 0 && n <= 15)
+	if (n < 0 || n > 15)
+		return;
+
+	for (i = 0; i <= n; i++)
 	{
-		for (i = 0; i <= n; i++)
+		for (j = 0; j <= n; j++)
 		{
-			for (j = 0; j <= n; j++)
+			int result = i * j;
+
+			if (j == 0)
+				_putchar('0');
+			else
 			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
+				_putchar(',');
+				_putchar(' ');
+
+				if (result < 10)
 					_putchar(' ');
+				if (result < 100)
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+
+				if (result >= 100)
+					_putchar(result / 100 + '0');
+				if (result >= 10)
+					_putchar((result / 10) % 10 + '0');
+
+				_putchar(result % 10 + '0');
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
