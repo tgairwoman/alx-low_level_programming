db8202b35e2162125aea107cdfa1432d92b1e323
tgairwoman <tgjehovah@gmail.com>
2023-08-10 00:07:42 +0300

    update

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,16 @@
 #include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(void)
 {
-    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+    _puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick\n");
+    printf("%c: %d\n", 'a', _isupper('a'));
+    char s1[] = "Hello";
+    char s2[] = "World!";
+
+    printf("%d\n", _strcmp(s1, s2));
+    printf("%d\n", _strcmp(s2, s1));
+    printf("%d\n", _strcmp(s1, s1));
     return (0);
 }
